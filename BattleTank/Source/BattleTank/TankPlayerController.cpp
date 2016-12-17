a08678ff1adeb13c13a68071e454e6108153a5fb
Sean Ryan <sryan@evercrack.com>
2016-12-16 23:01:22 -0500

    Add AI controller for tanks

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -11,3 +11,20 @@ ATank* ATankPlayerController::GetControlledTank() const
     return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    auto ControlledTank = GetControlledTank();
+    
+    if ( ! ControlledTank )
+    {
+        UE_LOG(LogTemp, Warning, TEXT("TankPlayerController not possesing a tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("TankPlayerController possesing [ %s ]"), *ControlledTank->GetName());
+    }
+
+}
+
